1b613fa82153cf4854665a206ce9a597a88f3d71
Vignesh KM <kmvignesh94@gmail.com>
2017-12-28 11:19:57 +0530

    Initial commit

---
+++ app/src/main/java/in/blogspot/kmvignesh/demoasynctask/Question.kt
@@ -0,0 +1,15 @@
+package `in`.blogspot.kmvignesh.demoasynctask
+
+/**
+ * Created by VickY on 2017-10-06.
+ */
+class Question{
+
+    var Question : String =""
+    var Option1: String =""
+    var Option2: String =""
+    var Option3: String =""
+    var Option4: String =""
+    var Answer: Int = 0
+
+}
